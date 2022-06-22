acd002ad51edaa0e51d754484a1c1dea1d5e24d7
Edgar Barrera <edgarlib123@gmail.com>
2022-06-21 21:31:23 -0500

    Initial Commit

---
+++ src/test/java/com/finvivir/pruebaeibg/PruebaeibgApplicationTests.java
@@ -0,0 +1,13 @@
+package com.finvivir.pruebaeibg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PruebaeibgApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
